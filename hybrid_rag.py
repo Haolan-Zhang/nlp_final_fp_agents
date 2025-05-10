7a9b62477c311430a40474081cf0cb4294b3fa6c
lans <haolanz@vt.edu>
2025-05-09 21:54:35 -0400

    initial commit

---
+++ hybrid_rag.py
@@ -0,0 +1,187 @@
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from langchain_core.documents import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.output_parser import StrOutputParser
+import pandas as pd
+import os
+os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
+
+# 1. Indexing and building chunks
+def get_chunks(md_file_path='doc.md', structure_loader=False, chunk_size=1000, chunk_overlap=100, separators=[r"(?=\n\d{4}(?:\.\d+)+\s)"]):
+    """
+    Function to split the content of a file into chunks.
+    
+    :param md_file_path: Path to the preprocessed markdown file.
+    :param structure_loader: Boolean indicating whether to use structured loader.
+    :param chunk_size: Size of each chunk.
+    :param chunk_overlap: Overlap between chunks.
+    :param separators: List of regex patterns to split the content.
+    :return: List of Document objects representing the chunks.
+    """
+    if structure_loader:
+        # Preserve the structure of the text
+        with open(md_file_path, "r", encoding="utf-8") as f:
+            structured_content = f.read()
+        # Create a Document manually
+        data = [Document(page_content=structured_content, metadata={"source": md_file_path})]
+    else:
+        # Load the markdown file without preserving structure
+        loader = UnstructuredMarkdownLoader(md_file_path)
+        data = loader.load()
+    
+    content = data[0].page_content
+    metadata = data[0].metadata
+
+    if separators: # by regex
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            separators=separators, # Regex to split documents by code index
+            is_separator_regex=True,
+            length_function=len
+        )
+    else: # by characters
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len
+        )
+    # Split the content into chunks
+    chunks = text_splitter.create_documents(texts=[content], metadatas=[metadata])
+    return chunks
+
+# 2. Creating hybrid retrievers
+def get_retrievers(chunks, k=3, weight_kw=0.5):
+    """
+    Function to create hybrid retrievers.
+    
+    :param md_split: List of Document objects representing the chunks.
+    :param k: Number of top documents to retrieve.
+    :param weight_kw: Weight for the keyword retriever.
+    :return: EnsembleRetriever object.
+    """
+    # Creating vector retriever
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(chunks, embeddings)
+    retriever = vector_store.as_retriever()
+
+    # Creating keyword retriever
+    keyword_retriever = BM25Retriever.from_documents(chunks)
+    keyword_retriever.k =  k
+
+    # Creating ensemble retriever
+    ensemble_retriever = EnsembleRetriever(retrievers=[retriever, keyword_retriever], weights=[1-weight_kw, weight_kw])
+    
+    return ensemble_retriever
+
+
+# 3. RAG chain
+def get_rag_chain(ensemble_retriever, template):
+    """
+    Function to create RAG chain.
+    
+    :param ensemble_retriever: EnsembleRetriever object.
+    :return: RAG chain object.
+    """
+    # Creating llm
+    llm = ChatOpenAI()
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    # Setup RAG pipeline
+    rag_chain = (
+        {"context": ensemble_retriever,  "input": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    
+    return rag_chain
+
+# 4. Evaluation
+def get_response(qa_list, rag_chain, ensemble_retriever, output_filename):
+    """
+    Function to get responses from the RAG chain and save them to a CSV file.
+    :param qa_list: List of dictionaries containing questions and answers.
+    :param rag_chain: RAG chain object.
+    :param ensemble_retriever: EnsembleRetriever object.
+    :param output_filename: Name of the output CSV file.
+    """
+    # create dataset 1
+    answers = [item["answer"] for item in qa_list]
+    questions = [item["question"] for item in qa_list]
+    response = []
+    contexts = []
+
+    # Inference
+    for query in questions:
+        response.append(rag_chain.invoke(query))
+        contexts.append([docs.page_content for docs in ensemble_retriever.get_relevant_documents(query)])
+
+    # To dict
+    data = {
+        "query": questions,
+        'answer': answers,
+        "response": response,
+        'manual_eval': None,
+        "context": contexts,
+    }
+    df = pd.DataFrame(data)
+    df.to_csv(f'{output_filename}.csv', index=False)
+
+
+
+if __name__ == "__main__":
+    # Template for QA prompting
+    template = """
+    You are a helpful assistant that answers questions strictly based on the provided context.
+    If the answer is not present in the context, respond with: "I don't know."
+
+    Context:
+    {context}
+
+    Question:
+    {input}
+
+    Answer:
+    """
+
+    # Sample expert set
+    from qa import qa_data_accessibility
+    # Sample amateur set
+    from qa import qa_data_accessibility_paraphrased
+
+    # Model 1: Hybrid RAG + raw text + char-based chunking
+    chunks = get_chunks(md_file_path='doc.md', structure_loader=False, separators=None)
+    ensemble_retriever = get_retrievers(chunks)
+    rag_chain = get_rag_chain(ensemble_retriever, template)
+    get_response(qa_data_accessibility, rag_chain, 'hybrid_raw_char_expert')
+    get_response(qa_data_accessibility_paraphrased, rag_chain, 'hybrid_raw_char_amateur')
+
+    # Model 2: Hybrid RAG + raw text + regex-based chunking
+    chunks = get_chunks(md_file_path='doc.md', structure_loader=False, separators=[r"(?=\n\d{4}(?:\.\d+)+\s)"])
+    ensemble_retriever = get_retrievers(chunks)
+    rag_chain = get_rag_chain(ensemble_retriever, template)
+    get_response(qa_data_accessibility, rag_chain, 'hybrid_raw_regex_expert')
+    get_response(qa_data_accessibility_paraphrased, rag_chain, 'hybrid_raw_regex_amateur')
+
+    # Model 3: Hybrid RAG + structured text + char-based chunking
+    chunks = get_chunks(md_file_path='doc.md', structure_loader=True, separators=None)
+    ensemble_retriever = get_retrievers(chunks)
+    rag_chain = get_rag_chain(ensemble_retriever, template)
+    get_response(qa_data_accessibility, rag_chain, 'hybrid_structured_char_expert')
+    get_response(qa_data_accessibility_paraphrased, rag_chain, 'hybrid_structured_char_amateur')
+
+    # Model 4: Hybrid RAG + structured text + regex-based chunking
+    chunks = get_chunks(md_file_path='doc.md', structure_loader=True, separators=[r"(?=\n\d{4}(?:\.\d+)+\s)"])
+    ensemble_retriever = get_retrievers(chunks)
+    rag_chain = get_rag_chain(ensemble_retriever, template)
+    get_response(qa_data_accessibility, rag_chain, 'hybrid_structured_regex_expert')
+    get_response(qa_data_accessibility_paraphrased, rag_chain, 'hybrid_structured_regex_amateur')
---
+++ qa.py
@@ -0,0 +1,169 @@
+qa_data_accessibility = [
+    {
+        "question": "What percentage of public entrances in a facility must be accessible?",
+        "answer": "At least 60 percent of all public entrances must be accessible."
+    },
+    {
+        "question": "How many accessible parking spaces are required for a parking facility with 200 total spaces?",
+        "answer": "A parking facility with 200 spaces must provide at least 6 accessible parking spaces."
+    },
+    {
+        "question": "Are accessible routes required from all site arrival points to the building entrance?",
+        "answer": "Yes, at least one accessible route is required from public transportation stops, accessible parking, and sidewalks to the building entrance."
+    },
+    {
+        "question": "What is the accessibility requirement for employee work areas?",
+        "answer": "Common use  circulation paths within employee work areas shall be accessible routes, unless they fall under specific exceptions outlined in the code."
+    },
+    {
+        "question": "When are press boxes exempt from being on an accessible route?",
+        "answer": "Press boxes are exempt if they are in bleachers with a single point of entry and under 500 sq ft, or if they are freestanding and over 12 feet above grade with an area under 500 sq ft."
+    },
+    {
+        "question": "What are the requirements for accessible electric vehicle charging stations?",
+        "answer": "At least 5% of vehicle spaces with EV charging must be accessible, and each must be 132 inches wide with a 60-inch access aisle."
+    },
+    {
+        "question": "How many wheelchair spaces are required in an assembly area with 500 seats?",
+        "answer": "At least 6 wheelchair spaces are required for assembly areas with 301 to 500 seats."
+    },
+    {
+        "question": "Are all dwelling units in Group R-2 required to be Type B units?",
+        "answer": "No, not all Group R-2 dwelling units are required to be Type B units. They are generally required when there are 4 or more units in a structure, but reductions are permitted under specific exceptions in Section 1108.7."
+    },
+    {
+        "question": "What exceptions exist for accessible routes within a site?",
+        "answer": "Accessible routes are not required between buildings or elements where access is only via vehicular routes or for some recreational facilities."
+    },
+    {
+        "question": "Are day care facilities in dwelling units fully subject to accessibility requirements?",
+        "answer": "No, only the portion of the structure used for the day care facility must comply with accessibility requirements."
+    }
+]
+
+qa_data_accessibility_paraphrased = [
+    {
+        "question": "How many of a building’s public entrances should be easy to use for people with disabilities?",
+        "answer": "At least 60 percent of all public entrances must be accessible."
+    },
+    {
+        "question": "If a parking lot has 200 spaces, how many of them need to be set up for people with disabilities?",
+        "answer": "A parking facility with 200 spaces must provide at least 6 accessible parking spaces."
+    },
+    {
+        "question": "Do there need to be paths that people with disabilities can use to get from the street or parking lot to the building?",
+        "answer": "Yes, at least one accessible route is required from public transportation stops, accessible parking, and sidewalks to the building entrance."
+    },
+    {
+        "question": "Do the hallways and walkways in employee-only areas need to be accessible?",
+        "answer": "Common use circulation paths within employee work areas shall be accessible routes, unless they fall under specific exceptions outlined in the code."
+    },
+    {
+        "question": "Are there situations where press boxes don’t have to be wheelchair-accessible?",
+        "answer": "Press boxes are exempt if they are in bleachers with a single point of entry and under 500 sq ft, or if they are freestanding and over 12 feet above grade with an area under 500 sq ft."
+    },
+    {
+        "question": "What are the rules for making electric car charging spots accessible?",
+        "answer": "At least 5% of vehicle spaces with EV charging must be accessible, and each must be 132 inches wide with a 60-inch access aisle."
+    },
+    {
+        "question": "How many spaces for wheelchairs are needed in a 490-seat event area?",
+        "answer": "At least 6 wheelchair spaces are required for assembly areas with 301 to 500 seats."
+    },
+    {
+        "question": "Do all apartments in a Group R-2 building need to be easy to use for people with disabilities?",
+        "answer": "No, not all Group R-2 dwelling units are required to be Type B units. They are generally required when there are 4 or more units in a structure, but reductions are permitted under specific exceptions in Section 1108.7."
+    },
+    {
+        "question": "Are accessible walkways needed between all buildings or areas on a site?",
+        "answer": "Accessible routes are not required between buildings or elements where access is only via vehicular routes or for some recreational facilities."
+    },
+    {
+        "question": "If a daycare is run out of a house, does the whole home have to follow accessibility rules?",
+        "answer": "No, only the portion of the structure used for the day care facility must comply with accessibility requirements."
+    }
+]
+
+
+qa_data_egress = [
+    {
+        "question": "What is the minimum ceiling height required for a means of egress?",
+        "answer": "The ceiling height for a means of egress must be at least 7 feet 6 inches above the finished floor."
+    },
+    {
+        "question": "How much can a protruding object extend below the minimum ceiling height in an egress path?",
+        "answer": "Protruding objects are allowed if a minimum headroom of 80 inches is provided over circulation paths."
+    },
+    {
+        "question": "What is the required occupant load factor for a business area?",
+        "answer": "The occupant load factor for a business area is 150 gross square feet per person."
+    },
+    {
+        "question": "How many accessible means of egress are required from an accessible space if more than one means of egress is required?",
+        "answer": "Each accessible portion must be served by not less than two accessible means of egress."
+    },
+    {
+        "question": "What is the minimum width required for stairways serving as part of an accessible means of egress?",
+        "answer": "Stairways must have a minimum clear width of 48 inches between handrails."
+    },
+    {
+        "question": "How long must emergency power provide illumination in the means of egress?",
+        "answer": "Emergency power must provide illumination for at least 90 minutes."
+    },
+    {
+        "question": "What is the maximum occupant load allowed per story before a third exit is required?",
+        "answer": "A third exit is required if the occupant load per story exceeds 500."
+    },
+    {
+        "question": "What are the required illumination levels under normal power in a means of egress?",
+        "answer": "The means of egress must have at least 1 footcandle of illumination at the walking surface."
+    },
+    {
+        "question": "What is the maximum allowable horizontal projection of objects between 27 inches and 80 inches above the floor?",
+        "answer": "Objects in this range must not project more than 4 inches into the circulation path."
+    },
+    {
+        "question": "What is the required size of each wheelchair space in an area of refuge?",
+        "answer": "Each wheelchair space must be at least 30 inches by 52 inches."
+    },
+    {
+        "question": "How should the separation distance between two required exits be measured?",
+        "answer": "The separation must be at least half the length of the maximum overall diagonal dimension of the area served."
+    },
+    {
+        "question": "Under what conditions can a single exit be permitted from a story?",
+        "answer": "A single exit may be permitted where occupant load and travel distances fall within limits of Table 1006.3.4(1) or (2)."
+    },
+    {
+        "question": "What is the minimum width required for egress components other than stairways based on occupant load?",
+        "answer": "0.2 inches per occupant is required, reduced to 0.15 inches if the building is sprinklered and has emergency alarms."
+    },
+    {
+        "question": "What is the purpose of a two-way communication system in areas of refuge?",
+        "answer": "It allows communication with a central control point or 911 in case of emergency evacuation needs."
+    },
+    {
+        "question": "When are elevators required as part of the accessible means of egress?",
+        "answer": "When a required accessible floor or occupied roof is four or more stories above or below the level of exit discharge."
+    },
+    {
+        "question": "Can elevators be used as a required component of egress?",
+        "answer": "No, except elevators used as an accessible means of egress in accordance with Section 1009.4."
+    },
+    {
+        "question": "What signage must be posted at doors to areas of refuge?",
+        "answer": "Each door must be identified with a sign stating 'AREA OF REFUGE' and comply with ICC A117.1."
+    },
+    {
+        "question": "What is the occupant load factor for concentrated assembly without fixed seats?",
+        "answer": "For concentrated assembly without fixed seats, the factor is 7 net square feet per person."
+    },
+    {
+        "question": "What emergency illumination level must be maintained at floor level during egress?",
+        "answer": "An average of 1 footcandle and a minimum of 0.1 footcandle must be maintained initially."
+    },
+    {
+        "question": "Are ramps acceptable for changes in elevation of less than 12 inches in the egress path?",
+        "answer": "Yes, ramps must be used when elevation change exceeds 6 inches, or floor materials should contrast for smaller changes."
+    }
+]
